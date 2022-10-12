ca4c21c3522c4a633a38bd958c9a1b2d51d1b9c4
Chalermchon Samana <tastsuya_desktop@hotmail.com>
2022-10-12 12:48:09 +0700

    Initial Commit

---
+++ convert/convertApp.swift
@@ -0,0 +1,17 @@
+//
+//  convertApp.swift
+//  convert
+//
+//  Created by Chalermchon COJ on 12/10/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct convertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
